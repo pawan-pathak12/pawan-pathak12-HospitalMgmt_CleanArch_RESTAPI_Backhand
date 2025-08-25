74b7b4a72d263aeeb295277214560b8b5740d126
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-08-25 19:41:44 +0545

    New Logic made in Appoitment but is incomplete

---
+++ HospitalManagment.Application/Features/Appointments/DTOs/SlotDto.cs
@@ -0,0 +1,8 @@
+﻿namespace HospitalManagment.Application.Features.Appointments.DTOs
+{
+    public class SlotDto
+    {
+        public TimeSpan AvailableTime { get; set; }
+        public TimeSpan EndTime { get; set; }
+    }
+}
