1a53e6c13a2769796e517ea388a5848b9ecdf108
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-09-23 21:02:32 +0545

    feat(appointment): add endpoint to get appointments by type (today, past, future) and date

---
+++ HospitalManagment.Application/Features/Appointments/Query/GetAllAppointmentsByDate/GetAllAppointmentByDateQueryHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using HospitalManagment.Application.Features.Appointments.DTOs;
+using HospitalManagment.Application.Interfaces;
+using MediatR;
+
+namespace HospitalManagment.Application.Features.Appointments.Query.GetAllAppointmentsByDate;
+
+public class
+    GetAllAppointmentByDateQueryHandler : IRequestHandler<GetAllAppointmentByDateQuery, IEnumerable<AppointmentDto>>
+{
+    private readonly IAppointmentRepository _appointmentRepository;
+    private readonly IMapper _mapper;
+
+    public GetAllAppointmentByDateQueryHandler(IAppointmentRepository appointmentRepository, IMapper mapper)
+    {
+        _appointmentRepository = appointmentRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<AppointmentDto>> Handle(GetAllAppointmentByDateQuery request,
+        CancellationToken cancellationToken)
+    {
+        var result = await _appointmentRepository.GetAppointmentsByDateAsync(request.Type, request.Date);
+        if (!result.Any()) throw new Exception($"Their is no Appointment on {request.Date}");
+        var appointments = _mapper.Map<IEnumerable<AppointmentDto>>(result);
+        return appointments;
+    }
+}
