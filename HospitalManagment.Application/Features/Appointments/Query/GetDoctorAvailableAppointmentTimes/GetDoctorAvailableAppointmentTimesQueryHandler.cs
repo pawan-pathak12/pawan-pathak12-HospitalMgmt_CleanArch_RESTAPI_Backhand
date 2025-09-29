83e98ca0745d431ac2a70d89871331cf9914241e
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-09-29 20:06:02 +0545

    Logic to give suggestion of avaialble slot is incomplete

---
+++ HospitalManagment.Application/Features/Appointments/Query/GetDoctorAvailableAppointmentTimes/GetDoctorAvailableAppointmentTimesQueryHandler.cs
@@ -0,0 +1,23 @@
+﻿using HospitalManagment.Application.Interfaces;
+using MediatR;
+
+namespace HospitalManagment.Application.Features.Appointments.Query.GetDoctorAvailableAppointmentTimes;
+
+public class GetDoctorAvailableAppointmentTimesQueryHandler : IRequestHandler<GetDoctorAvailableAppointmentTimesQuery>
+{
+    private readonly IDoctorRepository _doctorRepository;
+
+    public GetDoctorAvailableAppointmentTimesQueryHandler(IDoctorRepository doctorRepository)
+    {
+        _doctorRepository = doctorRepository;
+    }
+
+    public Task Handle(GetDoctorAvailableAppointmentTimesQuery request, CancellationToken cancellationToken)
+    {
+        //step 1 : get doctor Working Hour 
+
+        //step 2 : 
+
+        throw new NotImplementedException();
+    }
+}
