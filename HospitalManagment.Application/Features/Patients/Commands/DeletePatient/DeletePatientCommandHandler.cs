07cce0b6591a7529e4586690834a2ccec7229645
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-10-01 18:47:00 +0545

    refactor(patient:command,query): replaced exception with logger in all folder inside of patient : command , query

--- HospitalManagment.Application/Features/Patients/Commands/DeletePatient/DeletePatientCommandHandler.cs
+++ HospitalManagment.Application/Features/Patients/Commands/DeletePatient/DeletePatientCommandHandler.cs
@@ -1,22 +1,25 @@
-﻿using HospitalManagment.Application.Common;
-using HospitalManagment.Application.Interfaces;
+﻿using HospitalManagment.Application.Interfaces;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace HospitalManagment.Application.Features.Patients.Commands.DeletePatient;
 
 public class DeletePatientCommandHandler : IRequestHandler<DeletePatientCommad, Unit>
 {
+    private readonly ILogger<DeletePatientCommandHandler> _logger;
     private readonly IPatientRepository _repository;
 
-    public DeletePatientCommandHandler(IPatientRepository repository)
+    public DeletePatientCommandHandler(IPatientRepository repository, ILogger<DeletePatientCommandHandler> logger)
     {
         _repository = repository;
+        _logger = logger;
     }
 
     public async Task<Unit> Handle(DeletePatientCommad request, CancellationToken cancellationToken)
     {
         var patient = await _repository.GetByIdAsync(request.Id);
-        if (patient == null) throw new NotFoundException($"Their is no Patient with Id {request.Id}");
+        if (patient == null)
+            _logger.LogWarning($"Their is no Patient with Id {request.Id}");
         await _repository.DeleteAsync(request.Id);
         return Unit.Value;
     }
--- HospitalManagment.Application/Features/Patients/Commands/UpdatePatient/UpdatePatientCommandHandler.cs
+++ HospitalManagment.Application/Features/Patients/Commands/UpdatePatient/UpdatePatientCommandHandler.cs
@@ -1,27 +1,30 @@
 ﻿using AutoMapper;
-using HospitalManagment.Application.Common;
 using HospitalManagment.Application.Interfaces;
 using HospitalManagment.Domain.Entity;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace HospitalManagment.Application.Features.Patients.Commands.UpdatePatient;
 
 public class UpdatePatientCommandHandler : IRequestHandler<UpdatePatientCommand, Unit>
 {
+    private readonly ILogger<UpdatePatientCommandHandler> _logger;
     private readonly IMapper _mapper;
     private readonly IPatientRepository _repository;
 
-    public UpdatePatientCommandHandler(IPatientRepository repository, IMapper mapper)
+    public UpdatePatientCommandHandler(IPatientRepository repository, IMapper mapper,
+        ILogger<UpdatePatientCommandHandler> logger)
     {
         _repository = repository;
         _mapper = mapper;
+        _logger = logger;
     }
 
     public async Task<Unit> Handle(UpdatePatientCommand request, CancellationToken cancellationToken)
     {
         var result = await _repository.GetByIdAsync(request.Patient.Id);
-        if (result == null) throw new NotFoundException($"Their is no Patient with Id {request.Patient.Id}");
-
+        if (result == null)
+            _logger.LogWarning($"Their is no Patient with Id {request.Patient.Id}");
         var patient = _mapper.Map<Patient>(request.Patient);
         await _repository.UpdateAsync(request.Patient.Id, patient);
         return Unit.Value;
