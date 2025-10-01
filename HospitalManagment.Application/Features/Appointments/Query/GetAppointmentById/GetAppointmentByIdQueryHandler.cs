55db8cc84f43cda7be6d4dd7a04d2ec25d6c2e25
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-10-01 18:27:48 +0545

    refactor(appointment:query): replaced exception with logger in All query of Appointments

--- HospitalManagment.Application/Features/Appointments/Query/GetAppointmentById/GetAppointmentByIdQueryHandler.cs
+++ HospitalManagment.Application/Features/Appointments/Query/GetAppointmentById/GetAppointmentByIdQueryHandler.cs
@@ -1,26 +1,30 @@
 ﻿using AutoMapper;
-using HospitalManagment.Application.Common;
 using HospitalManagment.Application.Features.Appointments.DTOs;
 using HospitalManagment.Application.Interfaces;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace HospitalManagment.Application.Features.Appointments.Query.GetAppointmentById;
 
 public class GetAppointmentByIdQueryHandler : IRequestHandler<GetAppointmentByIdQuery, AppointmentDto>
 {
+    private readonly ILogger<GetAppointmentByIdQueryHandler> _logger;
     private readonly IMapper _mapper;
     private readonly IAppointmentRepository _repository;
 
-    public GetAppointmentByIdQueryHandler(IAppointmentRepository repository, IMapper mapper)
+    public GetAppointmentByIdQueryHandler(IAppointmentRepository repository, IMapper mapper,
+        ILogger<GetAppointmentByIdQueryHandler> logger)
     {
         _repository = repository;
         _mapper = mapper;
+        _logger = logger;
     }
 
     public async Task<AppointmentDto> Handle(GetAppointmentByIdQuery request, CancellationToken cancellation)
     {
         var appointment = await _repository.GetByIdAsync(request.Id);
-        if (appointment == null) throw new NotFoundException($"Their is no appointment with id {request.Id}.");
+        if (appointment == null)
+            _logger.LogWarning($"Their is no appointment with id {request.Id}.");
         var appointmentEntity = _mapper.Map<AppointmentDto>(appointment);
 
         return appointmentEntity;
