77efa5de9f8968d1350e4890fcce121b39acf4fe
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-10-01 18:47:00 +0545

    refactor(patient:command,query): replaced exception with logger in all folder inside of patient : command , query

--- HospitalManagment.Application/Features/Patients/Query/GetAllPatients/GetAllPatientQueryHandler.cs
+++ HospitalManagment.Application/Features/Patients/Query/GetAllPatients/GetAllPatientQueryHandler.cs
@@ -1,26 +1,30 @@
 ﻿using AutoMapper;
-using HospitalManagment.Application.Common;
 using HospitalManagment.Application.Features.Patients.DTOs;
 using HospitalManagment.Application.Interfaces;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace HospitalManagment.Application.Features.Patients.Query.GetAllPatients;
 
 public class GetAllPatientQueryHandler : IRequestHandler<GetAllPatientQuery, IEnumerable<PatientDto>>
 {
+    private readonly ILogger<GetAllPatientQueryHandler> _logger;
     private readonly IMapper _mapper;
     private readonly IPatientRepository _repository;
 
-    public GetAllPatientQueryHandler(IPatientRepository repository, IMapper mapper)
+    public GetAllPatientQueryHandler(IPatientRepository repository, IMapper mapper,
+        ILogger<GetAllPatientQueryHandler> logger)
     {
         _repository = repository;
         _mapper = mapper;
+        _logger = logger;
     }
 
     public async Task<IEnumerable<PatientDto>> Handle(GetAllPatientQuery request, CancellationToken cancellationToken)
     {
         var patients = await _repository.GetAllAsync();
-        if (patients == null || !patients.Any()) throw new NotFoundException("Patients data not found ");
+        if (patients == null || !patients.Any())
+            _logger.LogWarning("Patients data not found");
         var patientData = _mapper.Map<IEnumerable<PatientDto>>(patients);
         return patientData;
     }
--- HospitalManagment.Application/Features/Patients/Query/GetPatientById/GetPatientByIdQueryHandler.cs
+++ HospitalManagment.Application/Features/Patients/Query/GetPatientById/GetPatientByIdQueryHandler.cs
@@ -1,26 +1,30 @@
 ﻿using AutoMapper;
-using HospitalManagment.Application.Common;
 using HospitalManagment.Application.Features.Patients.DTOs;
 using HospitalManagment.Application.Interfaces;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace HospitalManagment.Application.Features.Patients.Query.GetPatientById;
 
 public class GetPatientByIdQueryHandler : IRequestHandler<GetPatientByIdQuery, PatientDto>
 {
+    private readonly ILogger<GetPatientByIdQueryHandler> _logger;
     private readonly IMapper _mapper;
     private readonly IPatientRepository _repository;
 
-    public GetPatientByIdQueryHandler(IPatientRepository repository, IMapper mapper)
+    public GetPatientByIdQueryHandler(IPatientRepository repository, IMapper mapper,
+        ILogger<GetPatientByIdQueryHandler> logger)
     {
         _repository = repository;
         _mapper = mapper;
+        _logger = logger;
     }
 
     public async Task<PatientDto> Handle(GetPatientByIdQuery request, CancellationToken cancellationToken)
     {
         var patient = await _repository.GetByIdAsync(request.Id);
-        if (patient == null) throw new NotFoundException($"Patient with id {request.Id} not found");
+        if (patient == null)
+            _logger.LogWarning($"Patient with id {request.Id} not found");
         var patientData = _mapper.Map<PatientDto>(patient);
         return patientData;
     }
