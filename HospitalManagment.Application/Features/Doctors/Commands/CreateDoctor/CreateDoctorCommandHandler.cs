1bfd2c6f71b2fb1d982d1653243d670f562c0b29
pawan-pathak12 <214448034+pawan-pathak12@users.noreply.github.com>
2025-10-01 18:41:20 +0545

    refactor(doctor:command,query): replaced exception with logger in all folder inside of doctor : command , query

--- HospitalManagment.Application/Features/Doctors/Commands/CreateDoctor/CreateDoctorCommandHandler.cs
+++ HospitalManagment.Application/Features/Doctors/Commands/CreateDoctor/CreateDoctorCommandHandler.cs
@@ -1,28 +1,28 @@
 ﻿using AutoMapper;
-using HospitalManagment.Application.Features.Doctors.DTOs;
 using HospitalManagment.Application.Interfaces;
 using HospitalManagment.Domain.Entity;
 using MediatR;
 
-namespace HospitalManagment.Application.Features.Doctors.Commands.CreateDoctor
+namespace HospitalManagment.Application.Features.Doctors.Commands.CreateDoctor;
+
+public class CreateDoctorCommandHandler : IRequestHandler<CreateDoctorCommand, Doctor>
 {
-    public class CreateDoctorCommandHandler : IRequestHandler<CreateDoctorCommand, Doctor>
+    private readonly IMapper _mapper;
+    private readonly IDoctorRepository _repository;
+
+    public CreateDoctorCommandHandler(IDoctorRepository repository, IMapper mapper)
     {
-        private readonly IDoctorRepository _repository;
-        private readonly IMapper _mapper;
+        _repository = repository;
+        _mapper = mapper;
+    }
 
-        public CreateDoctorCommandHandler(IDoctorRepository repository ,IMapper mapper)
-        {
-            _repository = repository;
-            this._mapper = mapper;
-        }
-        public async Task<Doctor> Handle(CreateDoctorCommand request, CancellationToken cancellationToken)
-        {
-            // Map CreateDoctorDto → Doctor entity
+    public async Task<Doctor> Handle(CreateDoctorCommand request, CancellationToken cancellationToken)
+    {
+        // add logic : if already same doctor entry is their with same field than block it 
+        // Map CreateDoctorDto → Doctor entity
 
-            var doctorEntity = _mapper.Map<Doctor>(request.Doctor);
+        var doctorEntity = _mapper.Map<Doctor>(request.Doctor);
 
-            return await _repository.AddAsync(doctorEntity);
-        }
+        return await _repository.AddAsync(doctorEntity);
     }
 }
